9583db96fe9aeaf0abeca8250c5ab1b1d2e4dad1
Karen Shea <shea.karene@gmail.com>
2019-01-09 17:25:14 -0500

    Adding .js files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//  testing:
+// const phrase = new Phrase();
+// const game = new Game();s
+
+//  testing:
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
